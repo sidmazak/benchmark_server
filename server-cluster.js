fdba4ad24e9364bfae3f018669d62425c5f99de3
siddmazak <sid@akvitek.com.au>
2025-10-06 11:28:27 +0530

    first commit

---
+++ server-cluster.js
@@ -0,0 +1,28 @@
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import express from 'express';
+
+const numCPUs = availableParallelism();
+
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    const app = express();
+    app.get('/', (req, res) => {
+        let sum = 0;
+        for (let i = 0; i < 1000000000; i++) {
+            sum += i;
+        }
+        res.send({
+            sum: sum
+        });
+    });
+    app.listen(3000, () => {
+        console.log(`Worker ${process.pid} started`);
+    });
+    console.log(`Worker ${process.pid} started`);
+}
---
+++ server-normal.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    let sum = 0;
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+    res.send({
+        sum: sum
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
