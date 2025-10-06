07aa034e3e4fa7c1eb053dac79b705884c3d86a9
siddmazak <sid@akvitek.com.au>
2025-10-06 11:58:06 +0530

    v3

--- server-normal.js
+++ server-normal.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import { cpus } from 'node:os';
 
 const app = express();
+
+// Function to get CPU core information
+function getCPUInfo() {
+    const cpuInfo = cpus();
+    return {
+        cores: cpuInfo.length,
+        model: cpuInfo[0]?.model || 'Unknown',
+        speed: cpuInfo[0]?.speed || 'Unknown'
+    };
+}
+
+const cpuInfo = getCPUInfo();
+console.log(`Single process server - PID: ${process.pid}, running on core ${process.pid % cpuInfo.cores}, CPU Model: ${cpuInfo.model}, Speed: ${cpuInfo.speed}MHz`);
 
 app.get('/', (req, res) => {
     let sum = 0;
@@ -13,5 +27,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port 3000 - PID: ${process.pid}, Core: ${process.pid % cpuInfo.cores}`);
 });
